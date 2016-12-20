bf081706ea28f93732fae26477174b92a20f73c2
arisnance <nance.aris@gmail.com>
2016-12-20 21:24:38 +0000

    integrate s3 storage

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,19 +1,21 @@
 #For retrieving the image from the remote url
 require 'open-uri' 
-
-
 class Blog < ActiveRecord::Base
+    
     belongs_to :user
     belongs_to :category
     
     has_attached_file :image,
-    :styles => { :medium => "300x300>",
-    :thumb => "100x100>" }
+                      :styles => { :medium => "300x300>",
+                      :thumb => "100x100>",
+                      :storage => :s3,
+                      :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
     
     validates_attachment_content_type :image,
     :content_type => /\Aimage\/.*\z/
+
+    def s3_credentials
+    {:bucket => 'lavail', :access_key_id => 'AKIAJBROJBFAAVVCH4KA', :secret_access_key => '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'}
     
-    def avatar_from_url(url)
-    self.avatar = open(url)
     end
 end
