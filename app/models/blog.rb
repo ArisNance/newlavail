251004805a5becd82ba9fac77fbbd929b03349ea
arisnance <nance.aris@gmail.com>
2016-12-20 21:31:13 +0000

    integrate s3 storage

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,13 +9,17 @@ class Blog < ActiveRecord::Base
                       :styles => { :medium => "300x300>",
                       :thumb => "100x100>",
                       :storage => :s3,
-                      :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                      :s3_credentials => Proc.new{|a| a.instance.s3_credentials
+                    }
     
     validates_attachment_content_type :image,
     :content_type => /\Aimage\/.*\z/
-
+    
+    def avatar_from_url(url)
+    self.avatar = open(url)
+    end
+    
     def s3_credentials
     {:bucket => 'lavail', :access_key_id => 'AKIAJBROJBFAAVVCH4KA', :secret_access_key => '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'}
-    
     end
 end
